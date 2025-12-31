1234e21f396256dee44c851d563b11a98423223d
Olaniyi <ayodelebwj@gmail.com>
2025-12-30 22:29:00 -0500

    adding to repo

--- jenkins-server/variables.tf
+++ jenkins-server/variables.tf
@@ -1,41 +1,41 @@
 variable "region" {
-    type = string
-    default = "us-east-2"
+  type    = string
+  default = "us-east-2"
 }
 
 
 variable "security_group_name" {
-    type = string
-    default = "jenkins-sg"
+  type    = string
+  default = "jenkins-sg"
 }
 
 
 
 variable "security_group_ingress_ssh_port" {
-    type = number
-    default = 22
+  type    = number
+  default = 22
 }
 
 variable "security_group_ingress_jenkins_port" {
-    type = number
-    default = 8080
+  type    = number
+  default = 8080
 }
 
 variable "security_group_cidr_block" {
-    type = string
-    default = "0.0.0.0/0"
+  type    = string
+  default = "0.0.0.0/0"
 }
 
 variable "jenkins_server_instance_type" {
-    type = string
-    default = "c7i-flex.large"
+  type    = string
+  default = "c7i-flex.large"
 }
 
 variable "jenkins_server_key_name" {
-    type = string
+  type = string
 }
 
 variable "jenkins_server_tag_name" {
-    type = string
-    default = "jenkins-instance"
+  type    = string
+  default = "jenkins-instance"
 }
