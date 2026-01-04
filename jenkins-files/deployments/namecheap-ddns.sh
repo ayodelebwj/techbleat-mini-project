70acb2d00d874153808b38dd88b4821978a6e50e
Olaniyi <ayodelebwj@gmail.com>
2026-01-03 23:02:02 -0500

    push

--- jenkins-files/deployments/namecheap-ddns.sh
+++ jenkins-files/deployments/namecheap-ddns.sh
@@ -6,4 +6,6 @@ DDNS_PASS="6ce19656be3d4fc1912474be7c228aeb"
 
 IP=$(curl -s https://api.ipify.org)
 
-curl "https://dynamicdns.park-your-domain.com/update?host=$HOST&domain=$DOMAIN&password=$DDNS_PASS&ip=$IP"
+for HOST in "@" "www" "*"; do
+  curl -s "https://dynamicdns.park-your-domain.com/update?host=$HOST&domain=$DOMAIN&password=$DDNS_PASS&ip=$IP"
+done
