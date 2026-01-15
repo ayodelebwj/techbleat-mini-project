dcd146dbbdf76758dc99ebf4ed4955707fd2cdf0
Olaniyi <ayodelebwj@gmail.com>
2026-01-14 23:47:27 -0500

    push to git

--- terraform-infrastructure/outputs.tf
+++ terraform-infrastructure/outputs.tf
@@ -3,18 +3,18 @@
 #======================================================
 #OUTPUTS PYTHON PUBLIC IP ADDRESS
 #======================================================
-output "python_instance_public_ip" {
-  description = "public ip for the python instance"
-  value       = aws_instance.python_instance.public_ip
-}
+#output "python_instance_public_ip" {
+ # description = "public ip for the python instance"
+ # value       = aws_instance.python_instance.public_ip
+#}
 
 #======================================================
 #OUTPUTS WEB PUBLIC IP ADDRESS
 #======================================================
-output "web_instance_public_ip" {
-  description = "public ip for the jenkins instance"
-  value       = aws_instance.web_instance.public_ip
-}
+#output "web_instance_public_ip" {
+ # description = "public ip for the jenkins instance"
+  #value       = aws_instance.web_instance.public_ip
+#}
 
 #======================================================
 #OUTPUTS JENKINS PUBLIC IP ADDRESS
