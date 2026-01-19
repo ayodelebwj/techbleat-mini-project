f74ab8cca102aebad9bc20aa8ce1644983822770
Olaniyi Kolawole <olaniyikolawole@MacBookPro.lan>
2026-01-19 03:20:06 -0500

    push

--- terraform-infrastructure/launchjenkins.sh
+++ terraform-infrastructure/launchjenkins.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 aws ssm start-session \
-  --target i-xxxxxxxx \
+  --target i-0c63a2185cadecf84 \
   --document-name AWS-StartPortForwardingSession \
   --parameters '{"portNumber":["8080"],"localPortNumber":["8080"]}'
