944d2e6e016094dc765ecf23de91e27a10a96e73
Olaniyi Kolawole <olaniyikolawole@MacBookPro.lan>
2026-01-19 08:05:45 -0500

    p

--- terraform-infrastructure/alb.tf
+++ terraform-infrastructure/alb.tf
@@ -16,36 +16,70 @@ resource "aws_lb" "internal_alb" {
 
 resource "aws_lb_target_group" "public_tg" {
   name     = "public-tg"
-  port     = 443
-  protocol = "HTTPS"
-  vpc_id   = aws_vpc.techbleatvpc.id
+  port        = 443
+  protocol    = "HTTPS"
+  vpc_id      = aws_vpc.techbleatvpc.id
+  target_type = "instance"
+
+  health_check {
+    protocol            = "HTTPS"
+    path                = "/"
+    port                = "traffic-port"
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 5
+    interval            = 30
+    matcher             = "200-399"
+  }
 }
 
 resource "aws_lb_target_group" "private_tg" {
   name     = "private-tg"
-  port     = 8000
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.techbleatvpc.id
+  port        = 8000
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.techbleatvpc.id
+  target_type = "instance"
+
+  health_check {
+    protocol            = "HTTP"
+    path                = "/"
+    port                = "traffic-port"
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 5
+    interval            = 30
+    matcher             = "200-399"
+  }
 }
 
 resource "aws_lb_listener" "public_listener" {
   load_balancer_arn = aws_lb.public_alb.arn
-  port              = 443
-  protocol          = "HTTPS"
+  port              = 80
+  protocol          = "HTTP"
 
   default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.public_tg.arn
+    type = "redirect"
+
+    redirect {
+      protocol = "HTTPS"
+      port     = "443"
+      status_code = "HTTP_301"
+    }
   }
 }
 
 resource "aws_lb_listener" "internal_listener" {
   load_balancer_arn = aws_lb.internal_alb.arn
-  port              = 443
-  protocol          = "HTTPS"
+  port              = 80
+  protocol          = "HTTP"
 
   default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.private_tg.arn
+    type = "redirect"
+
+    redirect {
+      protocol = "HTTPS"
+      port     = "443"
+      status_code = "HTTP_301"
+    }
   }
 }
